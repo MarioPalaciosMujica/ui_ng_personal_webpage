107599c8cfc79801cc0eab9dd8fceec8c44608dc
Mario Palacios <mario@Marios-iMac.local>
2021-03-27 16:49:23 -0300

    added ngx-translate with EN lang

--- personal-website/src/app/app.module.ts
+++ personal-website/src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { StoreModule } from '@ngrx/store';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HttpClient } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,6 +11,10 @@ import { authReducer } from '@core/reducers/auth.reducer';
 import { PublicComponent } from '@public/public.component';
 import { AdminComponent } from '@admin/admin.component';
 import { SharedModule } from '@shared/shared.module';
+
+export function HttpLoaderFactory(http: HttpClient){
+    return new TranslateHttpLoader(http, "./assets/i18n/", ".json");
+}
 
 @NgModule({
   declarations: [
@@ -20,7 +27,15 @@ import { SharedModule } from '@shared/shared.module';
     AppRoutingModule,
     SharedModule,
     StoreModule.forRoot({}),
-    StoreModule.forFeature('authReducer', authReducer)
+    StoreModule.forFeature('authReducer', authReducer),
+    TranslateModule.forRoot({
+        defaultLanguage: 'en',
+        loader: {
+            provide: TranslateLoader,
+            useFactory: HttpLoaderFactory,
+            deps: [HttpClient]
+        }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
